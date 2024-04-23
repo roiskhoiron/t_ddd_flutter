cc28cd8b06c309a2e3aaa52e115efa5f2012a524
roishoiron <rois.khoiron@gmail.com>
2024-04-23 16:54:35 +0700

    feat: Unit testing implementation in login_request.dart and login_response_test.dart

--- test/domain/entities/login_request_test.dart
+++ test/domain/entities/login_request_test.dart
@@ -1,29 +1,17 @@
-// buatkan saya pengetesan untuk class LoginRequest
 
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:t_ddd_flutter/domain/entities/login_request.dart';
 
-import 'login_request_test.mocks.dart';
-
-@GenerateMocks([LoginRequest])
 void main() {
-  group('LoginRequest', () {
-    late MockLoginRequest loginRequest;
+  test('LoginRequest should have the correct attributes', () {
+    final loginRequest = LoginRequest(
+      username: 'test@example.com',
+      password: 'password',
+    );
 
-    setUp(() {
-      loginRequest = MockLoginRequest();
-    });
-
-    test('LoginRequest should have the correct attributes', () {
-      final loginRequest = LoginRequest(
-        username: 'test@example.com',
-        password: 'password',
-      );
-
-      expect(loginRequest.username, 'test@example.com');
-      expect(loginRequest.password, 'password');
-    });
+    expect(loginRequest.username, 'test@example.com');
+    expect(loginRequest.password, 'password');
   });
 }
